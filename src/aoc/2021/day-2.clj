2103f8b4ff8afe2166ff231c5ee868eb674672f9
Robin Hekmatara <robin.hekmatara@evry.com>
2021-12-03 15:52:49 +0100

    Added solution for second day

---
+++ src/aoc/2021/day-2.clj
@@ -0,0 +1,25 @@
+(ns aoc.2021.day-2
+  (:require [clojure.string :as str]))
+
+(defn position
+  [{horizontal :horizontal depth :depth aim :aim}
+   [direction amount]]
+  (case direction
+    "forward" {:horizontal (+ horizontal amount) :depth (+ depth (* aim amount)) :aim aim}
+    "down" {:horizontal horizontal :depth depth  :aim (+ aim amount)}
+    "up" {:horizontal horizontal :depth  depth :aim (- aim amount)}))
+
+(defn end-position
+  [course]
+  (reduce position {:horizontal 0 :depth 0 :aim 0} course))
+
+(def slurp-file-to-list
+  (->>
+   (str/split (slurp "src/aoc/2021/day-2.txt") #"\n")
+   (map (fn [s] (str/split s #" ")))
+   (map (fn [[s1 s2]] [s1 (Integer/parseInt s2)]))))
+
+(comment
+  (end-position slurp-file-to-list))
+
+
